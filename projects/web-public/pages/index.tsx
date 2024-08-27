738dfd70a71eb56822b2f435b22116d4074e5c50
aline necchi <alinenecchi@gmail.com>
2024-08-27 08:33:06 -0300

    add packages

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { sum } from "@line/utils/math/sum";
 
 export default function HomeScreen() {
   return (
     <main>
       <h1>Home</h1>
+      <p>
+        Importando modulo local: @line/utils/math/sum sum(2,2) : {sum(2, 2)}
+      </p>
     </main>
   );
 }
